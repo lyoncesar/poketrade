441d95097828304578a750b6898262b56eee6604
Lyon Cesar <lyoncesar@gmail.com>
2020-12-22 11:07:56 -0300

    fix(bootstrap): Fix the integration with bootstrap.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,8 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    JQuery: 'jquery/src/jquery'
+    JQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
   })
 )
 
