a515f10b41d302d1376e688f731dd02d8a6b202e
Lyon Cesar <lyoncesar@gmail.com>
2020-12-23 17:26:59 -0300

    feat(exchange api): update fetch_pokemons_service
    Was changed the return of class from an array to class methods, and new
    tests was wroted

--- app/services/fetch_pokemons_service.rb
+++ app/services/fetch_pokemons_service.rb
@@ -1,52 +1,48 @@
 require 'poke-api-v2'
 
 class FetchPokemonsService
-  attr_reader :offering, :receiving
 
-  def initialize(offering, receiving)
-    @offering = offering
+  def initialize(sending, receiving)
+    @sending = sending
     @receiving = receiving
+    @unknown_list = []
   end
 
-  def call
-    updated_offering_and_receiving
+  def updated_send
+    validated_sending_list.compact
+  end
+
+  def updated_receive
+    validated_receiving_list.compact
+  end
+
+  def unknown_pokemons
+    updated_send
+    updated_receive
+
+    @unknown_list.compact
   end
 
   private
 
-  def fair_trade?
-    if (offering_experience - receiving_experience) > 10 || (offering_experience - receiving_experience) < -10
-      return false
-    end
+  attr_reader :sending, :receiving
 
-    true
+  def validated_sending_list
+    @validate_sendig_list ||= fetch_pokemons(sending)
   end
 
-  def offering_experience
-    calculate_xp(fetch_poke_offering)
+  def validated_receiving_list
+    @validated_receiving_list ||= fetch_pokemons(receiving)
   end
 
-  def receiving_experience
-    calculate_xp(fetch_poke_receiving)
-  end
+  def fetch_pokemons(poke_list)
+    poke_list.map do |name|
+      pokemon = poke_api_get(name)
 
-  def calculate_xp(xp_array)
-    xp = 0
-    xp_array.each { |poke| xp += poke.dig(:base_experience) }
-
-    xp
-  end
-
-  def updated_offering_and_receiving
-    {
-      offering: fetch_poke_offering,
-      receiving: fetch_poke_receiving
-    }
-  end
-
-  def fetch_poke_offering
-    offering.map do |offer|
-      pokemon = PokeApi.get(pokemon: offer.sub(/[$'$.]/, '').downcase.tr(' ', '-'))
+      if pokemon.blank?
+        @unknown_list << name unless @unknown_list.include?(name)
+        next
+      end
 
       {
         pokemon: pokemon.forms.first.name,
@@ -55,14 +51,11 @@ class FetchPokemonsService
     end
   end
 
-  def fetch_poke_receiving
-    receiving.map do |receive|
-      pokemon = PokeApi.get(pokemon: receive.underscore)
-
-      {
-        pokemon: pokemon.forms.first.name,
-        base_experience: pokemon.base_experience
-      }
+  def poke_api_get(name)
+    begin
+      PokeApi.get(pokemon: name.sub(/[$'$.]/, '').downcase.tr(' ', '-'))
+    rescue URI::InvalidURIError, JSON::ParserError
+      return
     end
   end
 end
--- spec/services/fetch_pokemons_service_spec.rb
+++ spec/services/fetch_pokemons_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe FetchPokemonsService do
-  let(:offering) {
+  let(:sending) {
     [
       "Farfetch'd",
       "Pikachu",
@@ -21,25 +21,69 @@ RSpec.describe FetchPokemonsService do
       "Drowzee"
     ]
   }
-  it 'return updated list of pokemons' do
-    service = described_class.new(offering, receiving)
-    expect(service.call).to eq({
-      offering: [
-        {base_experience: 132, pokemon:"farfetchd"},
-        {base_experience: 112, pokemon: "pikachu"},
-        {base_experience: 62, pokemon: "charmander"},
-        {base_experience: 122, pokemon: "wooloo"},
-        {base_experience: 227, pokemon: "nidoking"},
-        {base_experience: 158, pokemon: "venomoth"}
-      ],
-      receiving: [
-        {base_experience: 112, pokemon: "pikachu"},
-        {base_experience: 62, pokemon: "charmander"},
-        {base_experience: 179, pokemon: "sandaconda"},
-        {base_experience: 49, pokemon: "zubat"},
-        {base_experience: 225, pokemon: "alakazam"},
-        {base_experience: 66, pokemon: "drowzee"}
-      ]
-    })
+
+  context '#updated_send' do
+    it 'return the updated list of sended pokemons' do
+      service = described_class.new(sending, receiving)
+      expect(service.updated_send).to eq(
+        [
+          {base_experience: 132, pokemon:"farfetchd"},
+          {base_experience: 112, pokemon: "pikachu"},
+          {base_experience: 62, pokemon: "charmander"},
+          {base_experience: 122, pokemon: "wooloo"},
+          {base_experience: 227, pokemon: "nidoking"},
+          {base_experience: 158, pokemon: "venomoth"}
+        ]
+      )
+    end
+  end
+
+  context '#updated_receive' do
+    it 'return the updated list of received pokemons' do
+      service = described_class.new(sending, receiving)
+
+      expect(service.updated_receive).to eq(
+        [
+          {base_experience: 112, pokemon: "pikachu"},
+          {base_experience: 62, pokemon: "charmander"},
+          {base_experience: 179, pokemon: "sandaconda"},
+          {base_experience: 49, pokemon: "zubat"},
+          {base_experience: 225, pokemon: "alakazam"},
+          {base_experience: 66, pokemon: "drowzee"}
+        ]
+      )
+    end
+  end
+
+  context '#unknown_pokemons' do
+    it 'return the updated list of received pokemons' do
+      service = described_class.new(sending, receiving)
+
+      expect(service.unknown_pokemons).to eq([])
+    end
+
+    context 'when dont find a pokemon' do
+      let(:sending) {
+        [
+          "Unknown",
+          "Pikachu"
+        ]
+      }
+
+      let(:receiving) {
+        [
+          "Charmander",
+          "Unknown Pokemon"
+        ]
+      }
+
+      it 'return his name on unknown attribute' do
+        service = described_class.new(sending, receiving)
+
+        expect(service.updated_receive).to eq( [{ base_experience: 62, pokemon: "charmander" }] )
+        expect(service.updated_send).to eq( [{ base_experience: 112, pokemon: "pikachu" }] )
+        expect(service.unknown_pokemons).to eq( ["Unknown Pokemon", "Unknown"] )
+      end
+    end
   end
 end
