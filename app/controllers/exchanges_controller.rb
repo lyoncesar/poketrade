47f29fa1cbbefc5d272f300a0f3d82e09ebe3eb4
Lyon Cesar <lyoncesar@gmail.com>
2021-01-11 01:10:44 -0300

    feat(Create Trade service): Update the Controller.

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,9 +1,35 @@
 class ExchangesController < ApplicationController
+  def index
+    @exchanges = Exchange.all
+  end
+
   def new
     @exchange = Exchange.new
   end
 
   def create
-    binding.pry
+    contract = TradeContract.new.call(trade_params.to_h)
+
+    if contract.errors.blank?
+      adapter = TradeAdapter.new(contract.to_h)
+      @create_trade = CreateTradeService.new(adapter)
+
+      if @create_trade.call
+        flash[:success] = "A troca #{@create_trade.trade.id} foi registrada"
+        redirect_to exchanges_path(@create_trade.trade)
+      else
+        flash.now[:error] = "Não foi possível salvar a troca de pokemons."
+        render new_trade_path
+      end
+    else
+      flash.now[:error] = "Não foi possível salvar a troca de pokemons."
+      render new_trade_path
+    end
+  end
+
+  private
+
+  def trade_params
+    params.require(:exchange).permit(:avg_receiving, :avg_sending, receiving: [], sending: [])
   end
 end
